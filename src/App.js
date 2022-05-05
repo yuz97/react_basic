0e152c2be8f93e7c464e9bd22a9f01ce2345b0b6
yuz_97 <zhoumilau@gmail.com>
2022-05-05 12:26:33 +0800

    useEffect_&_fetch_data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App(props) {
 
+    const [users, setUsers] = useState([]);
+
+    const getUsers = async () => {
+        try {
+            let response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            console.log(response.data);
+            setUsers(response.data);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    return (
+        <div className="py-5">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Address</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    users.map((user, index) => {
+                                        return (
+                                            <tr key={user.id}>
+                                                <td>{++index}</td>
+                                                <td>{user.name}</td>
+                                                <td>{user.email}</td>
+                                                <td>{user.phone}</td>
+                                                <td>{user.address.street}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
